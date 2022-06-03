e965eddca901e256941c04498f75e5695f4af180
Justin <justin13601@hotmail.com>
2022-06-02 23:14:43 -0400

    added test cases

---
+++ tests/test_main.py
@@ -0,0 +1,18 @@
+def test_execute_experiment():
+    assert False
+
+
+def test_remove_correlated_features():
+    assert False
+
+
+def test_variance_threshold():
+    assert False
+
+
+def test_feature_selection():
+    assert False
+
+
+def test_preprocess_stanford():
+    assert False
